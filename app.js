18135977abf5e31c6936b0235f4b4918c5a2dd41
mohamadreza hedayat <mohamadrezahedayat@gmail.com>
2021-08-03 00:45:59 +0430

    implement cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,6 +24,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+app.options('*', cors());
+
 // SERVING STATIC FILES
 app.use(express.static(path.join(__dirname, 'public')));
 
